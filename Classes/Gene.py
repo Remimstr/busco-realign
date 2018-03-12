4a75ea18c26478a76b92dee25e06d51753298c89
remarcha <remievan@hotmail.com>
2018-03-12 10:09:46 -0400

    Choose best alignment

--- Classes/Gene.py
+++ Classes/Gene.py
@@ -1,5 +1,6 @@
 import os
 from Bio import SeqIO
+from Bio.Blast import NCBIXML
 
 import logging
 
@@ -9,16 +10,21 @@ class Gene:
     def __init__(self, gene, path):
         base = os.path.basename(gene)
         self.name = os.path.splitext(base)[0]
-        self.best = None
+        self.best_alignment = None
         self.record_files = self.split_records(gene, path)
+        self.alignment_files = []
 
     def __str__(self):
         ret_str = "For gene %s: " % self.name
-        if (self.best == None):
+        if not self.best_alignment:
             ret_str += "There is no best record yet"
         else:
             ret_str += self.records[self.best].__str__()
         return ret_str
+
+    def add_alignment_file(self, alignment_f):
+        if (alignment_f not in self.alignment_files):
+            self.alignment_files.append(alignment_f)
 
     # Takes a path, and a gene object and makes a new directory
     # for the gene at path, then splits the gene's records and places
@@ -36,3 +42,25 @@ class Gene:
             record_list.append(record_fasta_f)
         logger.info("Split gene %s into %s records" % (self.name, len(record_list)))
         return record_list
+
+    # Chooses the best record that exists, remove it from the list of
+    # record files and place it at "best"
+    def choose_best_match(self):
+        if not self.alignment_files:
+            raise RuntimeError("Please make a list of records before choosing a best match")
+        highest_b = 0
+        best_alignment = None
+        for alignment_f in self.alignment_files:
+            with open(alignment_f, "rU") as result_h:
+                try:
+                    record = NCBIXML.read(result_h)
+                    hsp = record.alignments[0].hsps[0]
+                    b = hsp.bits
+                    if (b > highest_b):
+                        highest_b = b
+                        best_alignment = alignment_f
+                except:
+                    logger.info("%s did not align, ignoring" % alignment_f)
+        self.alignment_files.remove(best_alignment)
+        self.best_alignment = best_alignment
+        logger.info("%s has a new best alignment: %s" % (self.name, self.best_alignment))
--- process.py
+++ process.py
@@ -30,13 +30,15 @@ def initial_alignment(container, directory):
     alignments_d = os.path.join(directory, "alignments")
     os.makedirs(alignments_d, exist_ok=True)
 
+
     for gene in container.genes:
         gene_alignments_d = os.path.join(alignments_d, gene.name)
         os.makedirs(gene_alignments_d, exist_ok=True)
         for record in gene.record_files:
             record_name = SeqIO.read(record, "fasta").name.replace(":", "-")
-            alignment_fasta_f = os.path.join(gene_alignments_d, record_name + ".xml")
+            alignment_f = os.path.join(gene_alignments_d, record_name + ".xml")
+            gene.add_alignment_file(alignment_f)
             # And then process the new file
-            if (not os.path.exists(alignment_fasta_f) or container.overwrite):
-                perform_alignment(container.assembly.record, record, alignment_fasta_f)
-        #choose_best_match(gene, o_file)
+            if (not os.path.exists(alignment_f) or container.overwrite):
+                perform_alignment(container.assembly.record, record, alignment_f)
+        gene.choose_best_match()
